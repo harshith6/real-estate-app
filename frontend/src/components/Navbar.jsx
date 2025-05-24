ed882b84a1f8e608c94bfe192eea1ad1bedbc221
unknown <harshithkc6@gmail.com>
2025-05-24 20:43:17 +0530

    deleting images

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -21,12 +21,12 @@ const Navbar = () => {
     <div className='absolute top-0 left-0 w-full z-10'>
         <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
             <img src={assets.logo}/>
-            <ul className='hidden md:flex gap-7 text-white'>
+            {/* <ul className='hidden md:flex gap-7 text-white'>
                 <a href='#Header' className='cursor-pointer hover:text-gray-400'>Home</a>
                 <a href='#About' className='cursor-pointer hover:text-gray-400'>about</a>
                 <a href='#Projects' className='cursor-pointer hover:text-gray-400'>Projects</a>
                 <a href='#Testimonials' className='cursor-pointer hover:text-gray-400'>Testimonials</a>
-            </ul>  
+            </ul>   */}
             {/* <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign up</button>  */}
             
 
@@ -40,13 +40,13 @@ const Navbar = () => {
             <div className='flex justify-end p-6 cursor-pointer'>
                 <img src={assets.cross_icon} alt="" className='w-6' onClick={()=>setShowMobileMenu(false)}/>
             </div>
-            <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-meduim'>
+            {/* <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-meduim'>
                 <a href="#Header" className='px-4 py-2 rounded-full inline-block' onClick={()=>setShowMobileMenu(false)} >Home</a>
                 <a href="#About" className='px-4 py-2 rounded-full inline-block'onClick={()=>setShowMobileMenu(false)}>About </a>
                 <a href="#Projects" className='px-4 py-2 rounded-full inline-block'onClick={()=>setShowMobileMenu(false)}>Projects</a>
                 <a href="#Testimonials" className='px-4 py-2 rounded-full inline-block'onClick={()=>setShowMobileMenu(false)}>Testimonials</a>
                 
-            </ul>
+            </ul> */}
         </div>
     </div>
   )
