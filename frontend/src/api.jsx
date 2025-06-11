a47eef6c1bd249e620e3bbd26f2352e79f777db1
harshith6 <68431329+harshith6@users.noreply.github.com>
2025-06-11 22:15:11 +0530

    Update api.jsx

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // Change if your backend URL is different
+  baseURL: '/api', // Change if your backend URL is different
 });
 
 export default api;
